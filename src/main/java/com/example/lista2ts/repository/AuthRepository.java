2fafb520e6b156427ea726b87a7b39ade384b3e0
Julian_laptop <268398@student.pwr.edu.pl>
2024-04-23 01:45:10 +0200

    Fixed missing Spring notification

--- src/main/java/com/example/lista2ts/repository/AuthRepository.java
+++ src/main/java/com/example/lista2ts/repository/AuthRepository.java
@@ -2,9 +2,11 @@ package com.example.lista2ts.repository;
 
 import com.example.lista2ts.entity.AuthEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthRepository extends JpaRepository<AuthEntity, Long> {
     Optional<AuthEntity> findByUsername(String username);
 }
